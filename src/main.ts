067e776859e203688a6bc8c649e6d2fa7c33f13c
Andy Burkovetsky <jamandyjam@gmail.com>
2020-10-01 22:31:34 +0300

    Add Logger

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import analyticsGoogle from './modules/analytics.google';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // auto-update module
-require('update-electron-app')();
+require('update-electron-app')({
+  // repo: 'github-user/repo',
+  // updateInterval: '1 hour',
+  logger: require('electron-log'),
+});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
