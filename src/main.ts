521fa13f0d975036abe28416f130a26707fff653
Andy Burkovetsky <jamandyjam@gmail.com>
2020-10-02 14:24:40 +0300

    Auto update  (#59)
    * Add auto-update 
    
    * 1.2.8

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { app, BrowserWindow, ipcMain } from 'electron';
 import { download } from 'electron-dl';
 import windowStateKeeper from 'electron-window-state';
 import analyticsGoogle from './modules/analytics.google';
+import logger from 'electron-log';
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // auto-update module
-require('update-electron-app')();
+require('update-electron-app')({
+  updateInterval: '1 hour',
+  logger,
+  // notifyUser: false,
+});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -58,7 +63,8 @@ const createWindow = () => {
       // allow main process be accesible in renderer via `remote` prop
       enableRemoteModule: true,
       // enable devtools only for dev mode
-      devTools: isDevMode,
+      // devTools: isDevMode,
+      devTools: true,
     },
     backgroundColor: '#ffffff',
     show: false,
