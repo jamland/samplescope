5bd5e9e8c104d16d2f8a85315a9bd3a32ee33df3
Andy Burkovetsky <jamandyjam@gmail.com>
2020-12-05 15:11:35 +0200

    Set custom window titlebar menu

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,8 +1,10 @@
 import { Menu, shell } from 'electron';
 
 interface Props {
-    appName: string;
+  appName: string;
 }
+
+const isDevMode = process.env?.DEV_MODE === 'true';
 
 /**
  * Module function to create main app menu.
@@ -10,98 +12,93 @@ interface Props {
  * but we want to modify menu items, so we write extended version for each
  */
 const appMenu = ({ appName }: Props) => {
-    const isMacOS = process.platform === 'darwin';
+  const isMacOS = process.platform === 'darwin';
 
-    // modified version of { role: 'appMenu' }
-    // Mac requires specific first item in menu with name of app
-    const appMenu = isMacOS ? {
+  // modified version of { role: 'appMenu' }
+  // Mac requires specific first item in menu with name of app
+  const appMenu = isMacOS
+    ? {
         label: appName,
         submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'services' },
-            { type: 'separator' },
-            { role: 'hide' },
-            { role: 'hideothers' },
-            { role: 'unhide' },
-            { type: 'separator' },
-            { role: 'quit' }
-        ]
-    }: {
+          { role: 'about' },
+          { type: 'separator' },
+          { role: 'services' },
+          { type: 'separator' },
+          { role: 'hide' },
+          { role: 'hideothers' },
+          { role: 'unhide' },
+          { type: 'separator' },
+          { role: 'quit' },
+        ],
+      }
+    : {
         label: 'App',
-        submenu: [
-            { role: 'quit' }
-        ]
-    }
+        submenu: [{ role: 'quit' }],
+      };
 
-    // TODO: Add Open Downloads folder item
-    // modified version of { role: 'viewMenu' }
-    const viewMenu = {
-        label: 'View',
-        submenu: [
-          { role: 'reload' },
-          { role: 'forceReload' },
-          { type: 'separator' },
-          { role: 'resetZoom' },
-          { role: 'zoomIn' },
-          { role: 'zoomOut' },
-          { type: 'separator' },
-          { role: 'togglefullscreen' }
-        ]
-    };
+  // TODO: Add Open Downloads folder item
+  // modified version of { role: 'viewMenu' }
+  const viewMenu = {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      ...(isDevMode && [{ role: 'toggleDevTools' }]),
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
+    ],
+  };
 
-    // modified version of { role: 'windowMenu' }
-    const windowMenu = {
-        label: 'Window',
-        submenu: [
-          { role: 'minimize' },
-          { role: 'zoom' },
-          ...(isMacOS ? [
+  // modified version of { role: 'windowMenu' }
+  const windowMenu = {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMacOS
+        ? [
             { type: 'separator' },
             { role: 'front' },
             { type: 'separator' },
-            { role: 'window' }
-          ] : [
-            { role: 'close' }
-          ])
-        ]
-    };
+            { role: 'window' },
+          ]
+        : [{ role: 'close' }]),
+    ],
+  };
 
-    // modified version of { role: 'helpMenu' }
-    const helpMenu = {
-        role: 'help',
-        submenu: [
-            {
-                label: 'Learn more',
-                click: () => {
-                    shell.openExternal('https://github.com/jamland/samplescope')
-                }
-            },
-            // TODO: add contacts later if it is required
-            // and What’s New…
-            // {
-            //     label: 'Contact',
-            // click: () => {
-            //     // open some chat group
-            // }
-            // }
-        ]
-    }
+  // modified version of { role: 'helpMenu' }
+  const helpMenu = {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn more',
+        click: () => {
+          shell.openExternal('https://github.com/jamland/samplescope');
+        },
+      },
+      // TODO: add contacts later if it is required
+      // and What’s New…
+      // {
+      //     label: 'Contact',
+      // click: () => {
+      //     // open some chat group
+      // }
+      // }
+    ],
+  };
 
-    // Menu template
-    const template: any[] = [
-        appMenu,
-        viewMenu,
-        windowMenu,
-        helpMenu,
-    ];
+  // Menu template
+  const template: any[] = [appMenu, viewMenu, windowMenu, helpMenu];
 
-    
-    // Build menu
-    const menu = Menu.buildFromTemplate(template);
+  // Build menu
+  const menu = Menu.buildFromTemplate(template);
 
-    // Set as main app menu
-    Menu.setApplicationMenu(menu);
+  // Set as main app menu
+  Menu.setApplicationMenu(menu);
 };
 
 export default appMenu;
