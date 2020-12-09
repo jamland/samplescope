fd73218b164970dfbcc522507a68f7fb7d560364
Andy Burkovetsky <jamandyjam@gmail.com>
2020-12-10 00:24:52 +0200

    Update free sound api key (#111)
    * Update FreeSound API key
    
    * Update packages
    
    * 1.3.4

--- src/renderer/hooks/useSampleSearch.tsx
+++ src/renderer/hooks/useSampleSearch.tsx
@@ -6,7 +6,9 @@ import { SampleList } from '@modules/freesound-search/freesound.types';
 
 const remote = window.require('electron').remote;
 const env = remote.getGlobal('process').env;
-const apiKey = env.SAMPLESCOPE_FREESOUND_API_KEY;
+const apiKey =
+  process.env.SAMPLESCOPE_FREESOUND_API_KEY ??
+  env.SAMPLESCOPE_FREESOUND_API_KEY;
 
 interface State {
   loading: boolean;
